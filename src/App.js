a6c9a38386804ec1c1796da7645c32c2065460cf
Yanabostyle <65418832+Yanabostyle@users.noreply.github.com>
2020-05-25 15:48:48 +0300

    added TopSlider component (#12)
    * added TopSlider component
    
    * merge conflicts
    
    * fix overflow

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import TopSlider from "./components/TopSlider/TopSlider";
 import Slider from './containers/SliderOfHitProducts/Slider';
 import './App.scss';
 import Footer from "./components/Footer/Footer";
@@ -8,6 +9,7 @@ const App = (props) => {
   return (
     <div className="App">
       <Header />
+      <TopSlider />
       <Slider />
       <Footer />
     </div>
