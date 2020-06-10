33c0d0a1822e52c7ef574ffc653676692b4a7b6c
Tsiupiak Myroslava <miradanko21@gmail.com>
2020-06-10 18:02:22 +0300

    intermediate option product-page

--- backend/config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Mira:Cpc6P5SOkaXLz4i5@cluster0-hsyog.azure.mongodb.net/ABCphoto?retryWrites=true&w=majority",
-  secretOrKey: "random very very secret string"
-};
