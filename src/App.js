f2f9fa01be66e09fdea20dafca3ff4a589c232f9
Tsiupiak Myroslava <miradanko21@gmail.com>
2020-06-22 22:24:20 +0300

    +Cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import TopSlider from "./components/TopSlider/TopSlider";
-import Slider from './containers/SliderOfHitProducts/Slider';
 import './App.scss';
 import Footer from "./components/Footer/Footer";
-import CardItem from "./components/Card/Card";
-import MainPage from "./pages/Main/Main";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Cameras from "./pages/Cameras/Cameras";
 import ProductPage from './pages/ProductPage/ProductPage';
@@ -22,7 +18,7 @@ const App = (props) => {
           <Route exact path="/" component={Home} />
           <Route exact path="/products/:itemNo" component={ProductPage} />
           <Route exact path='/cameras' component={Cameras} />
-            <Route exact path='/cart'  component={Cart}/>
+          <Route exact path='/cart'  component={Cart}/>
         </Switch>
         <Footer />
       </div>
