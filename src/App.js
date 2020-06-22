5c22fd6212178d4f090c7220f16b7b3ef30b93ac
Miradanko <50140647+Miradanko@users.noreply.github.com>
2020-06-22 23:07:20 +0300

    Product page version1 (#30)
    * test from button.js
    
    * button.test.js
    
    * buged currentProductReducer
    
    * fixed currentReducer
    
    * tabs->characteristics.js
    
    * redirect to productPage
    
    * images at hosting
    
    * characteristics.js fixed height
    
    * +Cart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import Header from './components/Header/Header';
-import TopSlider from "./components/TopSlider/TopSlider";
-import Slider from './containers/SliderOfHitProducts/Slider';
 import './App.scss';
 import Footer from "./components/Footer/Footer";
-import CardItem from "./components/Card/Card";
-import MainPage from "./pages/Main/Main";
-import {Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Cameras from "./pages/Cameras/Cameras";
 import ProductPage from './pages/ProductPage/ProductPage';
 import Home from './pages/Home/Home';
@@ -15,16 +11,18 @@ import Cart from "./pages/Cart/Cart";
 
 const App = (props) => {
   return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/product" component={ProductPage} />
-        <Route exact path='/cameras'  component={Cameras}/>
-        <Route exact path='/cart'  component={Cart}/>
-      </Switch>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products/:itemNo" component={ProductPage} />
+          <Route exact path='/cameras' component={Cameras} />
+          <Route exact path='/cart'  component={Cart}/>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
