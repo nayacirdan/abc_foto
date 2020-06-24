2c22dc441607cdbf45e706aa75754c023e2af91f
Tsiupiak Myroslava <miradanko21@gmail.com>
2020-06-24 16:30:04 +0300

    search filter with axios post on backend

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -2,9 +2,29 @@ import Actions from '../constans/constans';
 import axios from 'axios';
 
 export const getProducts = () => (dispatch) => {
-    // axios.get('/products.json')
     axios.get('/products')
         .then(res => {
             dispatch({ type: Actions.FETCH_PRODUCTS, payload: res.data })
         })
 };
+
+export const getFilteredProducts = (text) => (dispatch) => {
+    if(text.query) {
+        axios.post('/products/search', text)
+        .then(res => {
+            // const filtered = res.data.filter(item => {
+            //     return res.data.name.toLowerCase().includes(text.toLowerCase())
+            // })
+            dispatch({ type: Actions.FETCH_PRODUCTS, payload: res.data })
+        }) 
+        .catch(error => {
+            dispatch({ type: Actions.FETCH_PRODUCTS_FAILED, error })
+        })
+    } else {
+        dispatch(getProducts());
+    }
+};
+
+export const searchChange = (text) => (dispatch) => {
+    dispatch ({ type: Actions.SEARCH_CHANGE, payload: text })
+}
