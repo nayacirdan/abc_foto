88e0dac012ae315be19cef94892ef4cf0eb6f971
Yanabostyle <65418832+Yanabostyle@users.noreply.github.com>
2020-07-14 10:43:59 +0300

    added confirm order modal, header links changed, bugs fixed (#43)
    * added confirm order modal, header links changed, bugs fixed
    
    * fixed errors

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -2,12 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {Formik, Form} from 'formik';
 import FormInput from '../../components/FormInput/FormInput';
 import SelectInput from '../../components/SelectInput/SelectInput';
+import SimpleModal from '../../components/Modals/ConfirmOrderModal';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import TextField from '@material-ui/core/TextareaAutosize';
 import FormControl from '@material-ui/core/FormControl';
-import Button from '@material-ui/core/Button';
 import Box from '@material-ui/core/Box';
 import './CheckoutPage.scss';
 
@@ -36,7 +36,8 @@ const mockCart = [
 ];
 
 function CheckoutPage (props) {
-  const [value, setValue] = React.useState('text7');
+  const [deliveryMethod, setDeliveryMethod] = React.useState('text7');
+  const [paymentMethod, setPaymentMethod] = React.useState('text1');
   const [isActiveSelect, setIsActiveSelect] = useState(false);
   const [products, setProducts] = useState([]);
 
@@ -52,11 +53,17 @@ function CheckoutPage (props) {
     });
   }, []);
 
-  const handleChange = (event) => {
+  const handleChangeDeliveryMethod = (event) => {
     const {value} = event.target;
-    setValue(value);
+    setDeliveryMethod(value);
     setIsActiveSelect(true);
     if (value === 'text7') setIsActiveSelect(false);
+  };
+
+  const handleChangePaymentMethod = (event) => {
+    const {value} = event.target;
+    setPaymentMethod(value);
+    if (value === 'text1') setIsActiveSelect(false);
   };
 
   const validate = values => {
@@ -70,8 +77,15 @@ function CheckoutPage (props) {
       errors.email = 'Invalid email address';
     }
 
+    if (!values.phone) {
+      errors.phone = 'Required';
+    } else if (
+      !/^\d{10}$/i.test(values.phone)
+    ) {
+      errors.phone = 'Invalid phone number';
+    }
+
     if (!values.name) errors.name = 'Invalid name';
-    if (!values.phone) errors.phone = 'Invalid phone number';
 
     return errors;
   };
@@ -94,14 +108,14 @@ function CheckoutPage (props) {
                 </div>
                 <FormInput label="Имя получателя *" type="text" name="name" placeholder="Иван Петрович"/>
                 {errors.email && touched.email ? <div className="error-text">{errors.name}</div> : null}
-                <FormInput label="Телефон *" type="text" name="phone" placeholder="+38063 111 33 44"/>
+                <FormInput label="Телефон *" type="text" name="phone" placeholder="050 555 55 55"/>
                 {errors.email && touched.email ? <div className="error-text">{errors.phone}</div> : null}
                 <FormInput label="E-mail *" type="email" name="email" placeholder="Ivanov444@gmail.com"/>
                 {errors.email && touched.email ? <div className="error-text">{errors.email}</div> : null}
                 <div className="order-delivery">
                   <h4 className="big-text">Способы доставки</h4>
                   <FormControl component="fieldset">
-                    <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                    <RadioGroup aria-label="deliveryMethod" name="deliveryMethod" value={deliveryMethod} onChange={handleChangeDeliveryMethod}>
                       <FormControlLabel value="text7" control={<Radio/>} label="Самовывоз из пункта выдачи"/>
                       <SelectInput disabled={isActiveSelect}/>
                       <FormControlLabel value="text8" control={<Radio/>} label="Новая почта (в отделение)"/>
@@ -112,7 +126,7 @@ function CheckoutPage (props) {
                 <div className="order-payment">
                   <h4 className="big-text">Способы оплаты</h4>
                   <FormControl component="fieldset" className="radio-group">
-                    <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
+                    <RadioGroup aria-label="paymentMethod" name="paymentMethod" value={paymentMethod} onChange={handleChangePaymentMethod}>
                       <Box display="flex" flexDirection="row" justifyContent="space-between">
                         <Box textAlign="left">
                           <FormControlLabel value="text1" control={<Radio/>} label="Наличными при получении"/>
@@ -146,8 +160,7 @@ function CheckoutPage (props) {
                   </div>
                 ))}
                 <div className="order-sum">Сумма заказа:</div>
-                <Button className="checkout-container-button" variant="contained" color="#51AD33"
-                  onClick={handleSubmit}>Подтвердить</Button>
+                <SimpleModal/>
               </div>
             </div>
           </div>
