11e7b09d0236e90fcd3bfb2012c39d8b5b4bdc6b
nayacirdan <42298353+nayacirdan@users.noreply.github.com>
2020-07-22 03:08:52 +0300

    auth customer works with mock

--- src/pages/CheckoutPage/CheckoutPage.js
+++ src/pages/CheckoutPage/CheckoutPage.js
@@ -8,10 +8,16 @@ import {useDispatch, useSelector} from 'react-redux';
 import PersonalInfo from './PersonalInfo/PersonalInfo';
 import DeliverMethods from './DeliverMethods/DeliverMethods';
 import PaymentMethods from './PaymentMethods/PaymentMethods';
+import {Button} from '@material-ui/core';
+import axios from 'axios';
+import {productsNotJson} from "./c";
 
 function CheckoutPage () {
   const dispatch = useDispatch();
   const customer = useSelector(state => state.getCustomer.customerInfo);
+  const productsInCart = useSelector(state => state.cartReducer.cartInfo);
+  console.log(productsInCart);
+  console.log(customer._id);
   let customerName;
 
   if (customer.firstName || customer.lastName) {
@@ -19,16 +25,62 @@ function CheckoutPage () {
   }
 
   /* const getCartProducts = () => {
-    return new Promise((resolve, reject) => {
-      setTimeout(() => resolve(mockCart), 1000);
-    });
-  }; */
+      return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(mockCart), 1000);
+      });
+    }; */
 
   /* useEffect(() => {
-    getCartProducts().then(result => {
-      setProducts(result);
-    });
-  }, []); */
+      getCartProducts().then(result => {
+        setProducts(result);
+      });
+    }, []); */
+
+  const NAC_data = {
+    email: 'ksusha.cirdan@gmail.com',
+    mobile: '0937699607',
+    letterSubject: 'Thank you for order! You are welcome!',
+    letterHtml: '<h1>Your order is placed. OrderNo is 023689452.</h1><p>{Other details about order in your HTML}</p>',
+    canceled: false,
+    products: JSON.stringify(productsNotJson)
+  };
+
+  const AC_data = {
+    customerId: customer._id || '5f1318029ba9f42208815d10',
+    email: 'ksusha.cirdan@gmail.com',
+    mobile: '0937699607',
+    letterSubject: 'Thank you for order! You are welcome!',
+    letterHtml: '<h1>Your order is placed. OrderNo is 023689452.</h1><p>{Other details about order in your HTML}</p>',
+    canceled: false,
+    products: []
+  };
+
+  console.log(JSON.stringify(NAC_data));
+  console.log(JSON.stringify(AC_data));
+
+  const sendOrderNAC = () => {
+    axios.post('/orders', NAC_data)
+      .then(newOrder => {
+        console.log('NAC', newOrder.data);
+        /* Do something with newOrder */
+      })
+      .catch(err => {
+        console.log('NAC', err, err.message);
+        /* Do something with error, e.g. show error to user */
+      });
+  };
+
+  const sendOrderAC = () => {
+    axios.post('/orders', AC_data)
+      .then(newOrder => {
+        console.log('AC', newOrder.data);
+        /* Do something with newOrder */
+      })
+      .catch(err => {
+        console.log('AC', err, err.message);
+        /* Do something with error, e.g. show error to user */
+      });
+  };
 
   const handleSubmit = (values) => {
     dispatch(toggleOrderModal(true));
@@ -68,7 +120,7 @@ function CheckoutPage () {
           deliveryMethod: 'self',
           paymentMethod: 'cash'
         }}
-        onSubmit = {(values) => handleSubmit(values)}
+        onSubmit={(values) => handleSubmit(values)}
         validate={validate}
       >
         {({submitForm, values, errors, touched, handleSubmit}) => (
@@ -81,10 +133,11 @@ function CheckoutPage () {
 
                 {/* NOT WORKING, DONT RECEIVE VALUE */}
                 <Field
-                  component={TextField}
+                  as={TextField}
                   className="comments"
                   type="text"
-                  multiline rows={4}
+                  multiple
+                  rows={4}
                   placeholder="Комментарий"
                   variant="outlined"
                   name="comment"
@@ -94,6 +147,8 @@ function CheckoutPage () {
 
             {/* Items In Cart */}
             <SimpleModal onClick={submitForm}/>
+            <Button type='button' onClick={sendOrderNAC}>Не авторизированный</Button>
+            <Button type='button' onClick={sendOrderAC}>Авторизированный</Button>
           </div>
         )}
       </Formik>
---
+++ src/pages/CheckoutPage/c.js
@@ -0,0 +1,109 @@
+export const productsNotJson = [{
+  enabled: true,
+  imageUrls: ['https://res.cloudinary.com/miratsiupiak/image/upload/v1592831421/products/canon_2/image_2_uur7dj.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831421/products/canon_2/image_3_n53uc9.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831420/products/canon_2/image_1_wh8z5q.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831421/products/canon_2/image_5_a6gzhf.jpg'],
+  quantity: 1,
+  _id: '5ee0e3835ecb8a365496c723',
+  name: 'canon eos 6d mark ii body',
+  currentPrice: 32999,
+  previousPrice: 34699,
+  categories: 'photocameras',
+  productUrl: '/photocameras',
+  brand: 'Canon',
+  article: 54895152,
+  isAvailable: true,
+  description: 'Матрица CMOS 35.9x24 мм, 26.2 Мп / поддержка карт памяти SD/SDHC/SDXC / LCD-дисплей 3" / Full HD-видео / питание от литий-ионного аккумулятора / 144 x 110.5 x 74.8 мм, 765 г (включая аккумулятор и карту памяти)',
+  type: 'зеркальные',
+  matrix: 'CMOS',
+  isHit: true,
+  itemNo: '545096',
+  date: '2020-06-10T13:43:31.997Z',
+  __v: 0,
+  withLens: false,
+  color: 'черный',
+  cameraMatrix: ['35.9 x 24 мм CMOS', 'Количество эффективных пикселей: приблизительно 26.2 мегапикселя', 'Общее количество пикселей: приблизительно 27.1 мегапикселя', 'Соотношение сторон: 3:2', 'Фильтр нижних частот: встроенный/фиксированный', 'Очистка датчика изображения: встроенная система очистки EOS', 'Тип цветного светофильтра: основные цвета'],
+  classPro: 'Professional',
+  interfaceProp: ['GPS', 'Micro USB', 'NFC', 'WI-Fi'],
+  photosensitivity: ['Фото: авто (100 - 40000), 100 - 40000 (с шагом 1/3 ступени или целая ступень), в ручном режиме чувствительность ISO может быть расширена до L: 50, H1: 51200, H2: 102400', 'Видео: авто (100 - 25600), 100 - 25600 (с шагом 1/3 ступени или целая ступень), в автоматическом и ручном режиме чувствительность ISO может быть расширена до H1: 51200. H2: 102400'],
+  specialFeatures: ['NFC (соответствие NFC Forum Type 3/4 Tag (динамическое))'],
+  videoRecording: 'Full HD (1920x1080)',
+  guarantee: '24 месяца',
+  weight: 'Камера - 765 г (включая аккумулятор и карту памяти)',
+  label: 'canon eos 6d mark ii body',
+  value: 'canon eos 6d mark ii body'
+}, {
+  enabled: true,
+  imageUrls: ['https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_2_bgriub.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_3_v2gjof.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_4_vpylfh.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831004/products/canon_1/image_1_kw8g3l.jpg'],
+  quantity: 1,
+  _id: '5ee0e2115ecb8a365496c722',
+  name: 'canon eos 800d body black',
+  currentPrice: 15899,
+  categories: 'photocameras',
+  productUrl: '/photocameras',
+  brand: 'Canon',
+  article: 16875476,
+  isAvailable: true,
+  description: 'Матрица 22.3 x 14.9 мм, 24.2 Мп / поддержка карт памяти SD/SDHC/SDXC / Сенсорный ЖК-дисплей с переменным углом наклона Clear View II TFT 3" / FullHD-видео / питание от литий-ионного аккумулятора / 131 x 76.2 x 99.9 мм, 532 г',
+  type: 'зеркальные',
+  matrix: 'Полноразмерная',
+  isHit: true,
+  itemNo: '201312',
+  date: '2020-06-10T13:37:21.739Z',
+  __v: 0,
+  withLens: false,
+  color: 'черный',
+  cameraMatrix: ['22.3 x 14.9 мм CMOS', 'Эффективные пиксели: Прибл. 24.2 мегапикселя', 'Общее количество пикселей: Прибл. 25.8 мегапикселя', 'Соотношение сторон: 3:2'],
+  classPro: 'Professional',
+  exposureMode: ['Автоматический режим: доступен в режиме покадровой автофокусировки при оценочном замере по достижении фокусировки', 'Ручная: с помощью кнопки фиксации AE в режимах творческой зоны'],
+  lens: ['Совместимость со всеми объективами EF, EF-S'],
+  photosensitivity: ['Авто (100-25600), 100-25600 (при шаге 1/3 ступени или целая ступень)', 'Доступно расширение ISO до H: 51200', 'Во время съемки видео: Авто (100–12800), 100–12800 (при шаге 1/3 ступени или целая ступень)', 'чувствительность ISO может быть расширена до H: 25600'],
+  videoRecording: 'Full HD (1920x1080)',
+  interfaceProp: ['GPS', 'Micro USB', 'NFC', 'WI-Fi'],
+  guarantee: '24 месяца официальной гарантии от производителя',
+  specialFeatures: ['NFC'],
+  weight: '532 г (включая аккумулятор и карту памяти)',
+  catalogs: 'digital-techniques',
+  label: 'canon eos 800d body black',
+  value: 'canon eos 800d body black'
+}, {
+  enabled: true,
+  imageUrls: ['https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_2_bgriub.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_3_v2gjof.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831011/products/canon_1/image_4_vpylfh.jpg', 'https://res.cloudinary.com/miratsiupiak/image/upload/v1592831004/products/canon_1/image_1_kw8g3l.jpg'],
+  quantity: 1,
+  _id: '5ee0e2115ecb8a365496c722',
+  name: 'canon eos 800d'
+}
+];
+
+
+const values = {
+  email: 'ksusha.cirdan@gmail.com',
+  phone: '0937699607',
+  comment: 'апвапвап',
+  deliveryMethod: 'self',
+  paymentMethod: 'cash'
+};
+const sendData = (values) => {
+
+};
+const NAC_data = {
+  email: values.email,
+  mobile: values.phone,
+  comment: values.comment,
+  deliveryMethod: values.deliveryMethod,
+  paymentMethod: values.paymentMethod,
+  letterSubject: 'Thank you for order! You are welcome!',
+  letterHtml: '<h1>Your order is placed. OrderNo is 023689452.</h1><p>{Other details about order in your HTML}</p>',
+  canceled: false,
+  products: JSON.stringify(productsNotJson)
+};
+
+const AC_data = {
+  email: values.email,
+  mobile: values.phone,
+  comment: values.comment,
+  deliveryMethod: values.deliveryMethod,
+  paymentMethod: values.paymentMethod,
+  letterSubject: 'Thank you for order! You are welcome!',
+  letterHtml: '<h1>Your order is placed. OrderNo is 023689452.</h1><p>{Other details about order in your HTML}</p>',
+  canceled: false,
+  products: []
+};
