3cc109dfdc1c5411a513fb63b13edb8dbcac911b
Oksana <42298353+nayacirdan@users.noreply.github.com>
2020-07-22 18:30:20 +0300

    checkout cart items (#68)

--- src/pages/CheckoutPage/ItemsInCart/ItemsInCart.js
+++ src/pages/CheckoutPage/ItemsInCart/ItemsInCart.js
@@ -1,29 +1,20 @@
 import React from 'react';
 import SimpleModal from '../../../components/Modals/ConfirmOrderModal';
+import CartItems from '../../Cart/CartItems/CartItems';
 
-const MyComponent = ({submitForm, products}) => {
+const ItemsInCart = ({submitBtnFunc}) => {
   return (
     <div>
       <div className="big-text checkout-title">Товары в корзине</div>
       <div className="checkout-container">
-        {products.map((product, i) => (
-          <div className="checkout-item" key={`${product.imageURL}${i}`}>
-            <img className="checkout-item-image" src={product.imageURL}
-              alt="checkout-item"/>
-            <div className="item-description">
-              <span className="item-description-title">{product.name}</span>
-              <span className="item-description-code">Код товара: {product.article}</span>
-            </div>
-            <div className="checkout-item-price">{product.price}</div>
-          </div>
-        ))}
-        <div className="order-sum">Сумма заказа: 41 397 грн </div>
-
+        <CartItems/>
+        {/* <div className="order-sum">Сумма заказа: 41 397 грн </div> */}
+          
         {/* Modal + Submit Button */}
-        <SimpleModal onClick={submitForm}/>
+        <SimpleModal onClick={submitBtnFunc}/>
       </div>
     </div>
   );
 };
 
-export default MyComponent;
+export default ItemsInCart;
